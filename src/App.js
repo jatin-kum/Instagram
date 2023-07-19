8f629943fc1bca6ecf89557d87ebddddb1e2d4e7
Jatin Kumar <jk9927729975@gmail.com>
2023-07-19 13:33:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
+import React, { createContext, useState } from "react";
 import './App.css';
+import Navbar from "./Components/Navbar";
+import{ BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./screens/Home";
+import SignUp from "./Components/SignUp";
+import SignIn from './Components/SignIn';
+import Profile from './screens/Profile';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Createpost from './screens/Createpost';
+import { LoginContext } from "./context/LoginContext";
+import Modal from "./Components/Modal";
+import UserProfie from "./Components/UserProfile";
+import MyFollowingPost from "./screens/MyFollowingPost";
+// mongoose.set('strictQuery',true);
 
 function App() {
+  const [userLogin, setUserLogin] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">  
+      <LoginContext.Provider value={{ setUserLogin, setModalOpen }}>
+        <Navbar login={userLogin}/> 
+        <Routes> 
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route> 
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/createPost" element={<Createpost/>}></Route>
+          <Route path="/profile/:userid" element={<UserProfie />}></Route>
+          <Route path="/myfollowingpost" element={<MyFollowingPost />}></Route>
+        </Routes>
+        <ToastContainer theme='dark'/>
+        </LoginContext.Provider>
+        {modalOpen && <Modal setModalOpen={setModalOpen}></Modal>}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import logo from '../img/logo.webp';
+import '../css/Navbar.css'
+import {Link} from "react-router-dom";
+import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from 'react-router-dom';
+
+export default function Navbar({ login }) {
+  const navigate =useNavigate()
+  const { setModalOpen } = useContext(LoginContext);
+
+  const loginStatus = () => {
+    const token = localStorage.getItem("jwt");
+    if (login || token) {
+      return [
+        <>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+          <Link to="/createPost">Create Post</Link>
+          <Link style={{ marginLeft: "20px" }} to="/myfollowingpost">
+            My Following
+          </Link>
+          <Link to={""}>
+            <button className="primaryBtn" onClick={() => setModalOpen(true)}>
+              Log Out
+            </button>
+          </Link>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/signin">
+            <li>SignIn</li>
+          </Link>
+        </>,
+      ];
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" onClick={()=>{navigate("/")}}/>
+      <ul className="nav-menu">{loginStatus()}</ul>
+    </div>
+  );
+}
